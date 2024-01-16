c4503891cf399b1bfbf8dc54b7518dec775ada4d
Caio Ribeiro de Carvalho <71287644+CaioCarvalho314@users.noreply.github.com>
2024-01-16 16:20:25 -0300

    feat: text editor added

--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -1,25 +1,25 @@
-import clsx from 'clsx'
-import { Code, CaretDoubleRight, TrashSimple } from 'phosphor-react'
-import * as Breadcrumbs from './Breadcrumbs'
+import clsx from "clsx";
+import { Code, CaretDoubleRight, TrashSimple } from "phosphor-react";
+import * as Breadcrumbs from "./Breadcrumbs";
 
 export function Header() {
-  const isMacOS = process.platform === 'darwin'
-  const isSidebarOpen = true
+  const isMacOS = process.platform === "darwin";
+  const isSidebarOpen = true;
 
   return (
     <div
       id="header"
       className={clsx(
-        'border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
+        "border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag",
         {
-          'pl-24': !isSidebarOpen && isMacOS,
-          'w-screen': !isSidebarOpen,
-          'w-[calc(100vw-240px)]': isSidebarOpen,
-        },
+          "pl-24": !isSidebarOpen && isMacOS,
+          "w-screen": !isSidebarOpen,
+          "w-[calc(100vw-240px)]": isSidebarOpen,
+        }
       )}
     >
       <button
-        className={clsx('h-5 w-5 text-rotion-200 hover:text-rotion-50', {
+        className={clsx("h-5 w-5 text-rotion-200 hover:text-rotion-50", {
           hidden: isSidebarOpen,
           block: !isSidebarOpen,
         })}
@@ -36,7 +36,7 @@ export function Header() {
           <Breadcrumbs.Separator />
           <Breadcrumbs.HiddenItems />
           <Breadcrumbs.Separator />
-          <Breadcrumbs.Item>Back-end</Breadcrumbs.Item>
+          <Breadcrumbs.Item>Back-End</Breadcrumbs.Item>
           <Breadcrumbs.Separator />
           <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
         </Breadcrumbs.Root>
@@ -49,5 +49,5 @@ export function Header() {
         </div>
       </>
     </div>
-  )
+  );
 }
