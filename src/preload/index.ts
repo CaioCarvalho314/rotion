4a88e37de041cf48cb96dc9f42180b6a4080c704
Caio Ribeiro de Carvalho <71287644+CaioCarvalho314@users.noreply.github.com>
2024-01-20 16:41:14 -0300

    feat: add delete function

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,6 +1,7 @@
-import { contextBridge, ipcRenderer } from "electron";
-import { ElectronAPI, electronAPI } from "@electron-toolkit/preload";
-import { IPC } from "../shared/constants/ipc";
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { contextBridge, ipcRenderer } from 'electron'
+import { ElectronAPI, electronAPI } from '@electron-toolkit/preload'
+import { IPC } from '../shared/constants/ipc'
 import {
   FetchAllDocumentsResponse,
   FetchDocumentResponse,
@@ -8,47 +9,47 @@ import {
   CreateDocumentResponse,
   SaveDocumentRequest,
   DeleteDocumentRequest,
-} from "../shared/@types/ipc";
+} from '../shared/@types/ipc'
 
 declare global {
   export interface Window {
-    electron: ElectronAPI;
-    api: typeof api;
+    electron: ElectronAPI
+    api: typeof api
   }
 }
 
 // Custom APIs for renderer
 const api = {
   fetchDocuments(): Promise<FetchAllDocumentsResponse> {
-    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL);
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH_ALL)
   },
   fetchDocument(req: FetchDocumentRequest): Promise<FetchDocumentResponse> {
-    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, req);
+    return ipcRenderer.invoke(IPC.DOCUMENTS.FETCH, req)
   },
   createDocument(): Promise<CreateDocumentResponse> {
-    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE);
+    return ipcRenderer.invoke(IPC.DOCUMENTS.CREATE)
   },
   saveDocument(req: SaveDocumentRequest): Promise<CreateDocumentResponse> {
-    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req);
+    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req)
   },
   deleteDocument(req: DeleteDocumentRequest): Promise<void> {
-    return ipcRenderer.invoke(IPC.DOCUMENTS.SAVE, req);
+    return ipcRenderer.invoke(IPC.DOCUMENTS.DELETE, req)
   },
-};
+}
 
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld("electron", electronAPI);
-    contextBridge.exposeInMainWorld("api", api);
+    contextBridge.exposeInMainWorld('electron', electronAPI)
+    contextBridge.exposeInMainWorld('api', api)
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 } else {
   // @ts-ignore (define in dts)
-  window.electron = electronAPI;
+  window.electron = electronAPI
   // @ts-ignore (define in dts)
-  window.api = api;
+  window.api = api
 }
--- src/renderer/src/components/Header/index.tsx
+++ src/renderer/src/components/Header/index.tsx
@@ -1,29 +1,48 @@
-import clsx from "clsx";
-import { Code, CaretDoubleRight, TrashSimple } from "phosphor-react";
-import * as Breadcrumbs from "./Breadcrumbs";
-import * as Collapsible from "@radix-ui/react-collapsible";
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import clsx from 'clsx'
+import { Code, CaretDoubleRight, TrashSimple } from 'phosphor-react'
+import * as Breadcrumbs from './Breadcrumbs'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { Document } from '~/src/shared/@types/ipc'
 
 interface HeaderProps {
-  isSidebarOpen: boolean;
+  isSidebarOpen: boolean
 }
 
 export function Header({ isSidebarOpen }: HeaderProps) {
-  const isMacOS = process.platform === "darwin";
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+  const { id } = useParams<{ id: string }>()
+  const isMacOS = process.platform === 'darwin'
+
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: async () => {
+      await window.api.deleteDocument({ id: id! })
+    },
+    onSuccess: () => {
+      queryClient.setQueryData(['documents'], (prevDocuments: Document[]) => {
+        return prevDocuments.filter((document) => document.id !== id)
+      })
+      navigate('/')
+    },
+  })
 
   return (
     <div
       id="header"
       className={clsx(
-        "border-b border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag",
+        'border-b h-14 border-rotion-600 py-[1.125rem] px-6 flex items-center gap-4 leading-tight transition-all duration-250 region-drag',
         {
-          "pl-24": !isSidebarOpen && isMacOS,
-          "w-screen": !isSidebarOpen,
-          "w-[calc(100vw-240px)]": isSidebarOpen,
-        }
+          'pl-24': !isSidebarOpen && isMacOS,
+          'w-screen': !isSidebarOpen,
+          'w-[calc(100vw-240px)]': isSidebarOpen,
+        },
       )}
     >
       <Collapsible.Trigger
-        className={clsx("h-5 w-5 text-rotion-200 hover:text-rotion-50", {
+        className={clsx('h-5 w-5 text-rotion-200 hover:text-rotion-50', {
           hidden: isSidebarOpen,
           block: !isSidebarOpen,
         })}
@@ -31,27 +50,35 @@ export function Header({ isSidebarOpen }: HeaderProps) {
         <CaretDoubleRight className="h-4 w-4" />
       </Collapsible.Trigger>
 
-      <>
-        <Breadcrumbs.Root>
-          <Breadcrumbs.Item>
-            <Code weight="bold" className="h-4 w-4 text-pink-500" />
-            Estrutura técnica
-          </Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.HiddenItems />
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item>Back-End</Breadcrumbs.Item>
-          <Breadcrumbs.Separator />
-          <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
-        </Breadcrumbs.Root>
+      {id ? (
+        <>
+          <Breadcrumbs.Root>
+            <Breadcrumbs.Item>
+              <Code weight="bold" className="h-4 w-4 text-pink-500" />
+              Estrutura técnica
+            </Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.HiddenItems />
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item>Back-End</Breadcrumbs.Item>
+            <Breadcrumbs.Separator />
+            <Breadcrumbs.Item isActive>Untitled</Breadcrumbs.Item>
+          </Breadcrumbs.Root>
 
-        <div className="inline-flex region-no-drag">
-          <button className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50">
-            <TrashSimple className="h-4 w-4" />
-            Apagar
-          </button>
-        </div>
-      </>
+          <div className="inline-flex region-no-drag">
+            <button
+              onClick={() => mutateAsync()}
+              disabled={isPending}
+              className="inline-flex items-center gap-1 text-rotion-100 text-sm hover:text-rotion-50 disabled:opacity-60"
+            >
+              <TrashSimple className="h-4 w-4" />
+              Apagar
+            </button>
+          </div>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
-  );
+  )
 }
